2a053ee71a6d1725b01b8dd3baafb5da003a1578
Rui Morais <rui.morais@pm.me>
2021-05-12 14:34:16 +0100

    Update to Ciris 2.x

--- src/main/scala/ciris/SecretInNamespace.scala
+++ src/main/scala/ciris/SecretInNamespace.scala
@@ -1,23 +1,22 @@
 package ciris.kubernetes
 
-import cats.effect.Blocker
 import ciris.ConfigValue
 import io.kubernetes.client.openapi.ApiClient
 
-sealed abstract class SecretInNamespace {
-  def apply(name: String): ConfigValue[String]
+sealed abstract class SecretInNamespace[F[_]] {
+  def apply(name: String): ConfigValue[F, String]
 
-  def apply(name: String, key: String): ConfigValue[String]
+  def apply(name: String, key: String): ConfigValue[F, String]
 }
 
 private[kubernetes] final object SecretInNamespace {
-  final def apply(namespace: String, client: ApiClient, blocker: Blocker): SecretInNamespace =
-    new SecretInNamespace {
-      override final def apply(name: String): ConfigValue[String] =
-        secret(client, name, namespace, None, blocker)
+  final def apply[F[_]](namespace: String, client: ApiClient): SecretInNamespace[F] =
+    new SecretInNamespace[F] {
+      override final def apply(name: String): ConfigValue[F, String] =
+        secret(client, name, namespace, None)
 
-      override final def apply(name: String, key: String): ConfigValue[String] =
-        secret(client, name, namespace, Some(key), blocker)
+      override final def apply(name: String, key: String): ConfigValue[F, String] =
+        secret(client, name, namespace, Some(key))
 
       override final def toString: String =
         s"SecretInNamespace($namespace)"
